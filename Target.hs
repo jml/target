3b70e33da54ee6b6e4db8837223e12f1bb91ab58
Jonathan Lange <jml@canonical.com>
2011-12-19 10:00:35 +0000

    Haskell version.

--- Target.hs
+++ Target.hs
@@ -1,5 +1,8 @@
 import Data.List
+import Data.Word
+import qualified Data.ByteString.Lazy as B
 import System.Environment (getArgs)
+import System.IO
 
 
 wordFile = "/home/jml/Dropbox/Code/Scrabble/source_data/sowpods.txt"
@@ -8,21 +11,30 @@ isShuffledSublist bigger (x:xs) | x `elem` bigger = isShuffledSublist (delete x 
                                 | otherwise = False
 isShuffledSublist _ _ = True
 
+
+c2w :: Char -> Word8
+c2w = fromIntegral . fromEnum
+
+newline = c2w '\n'
+byteLines = B.split newline
+byteUnlines = B.intercalate (B.pack [newline])
+
+
+fitsTarget :: [Word8] -> Word8 -> B.ByteString -> Bool
 fitsTarget targetWord central word =
     and [wordLength >= 4,
-         wordLength <= targetWordLength,
-         central `elem` word,
-         isShuffledSublist targetWord word]
+         wordLength <= fromIntegral targetWordLength,
+         central `B.elem` word,
+         isShuffledSublist targetWord (B.unpack word)]
     where
-      wordLength = length word
+      wordLength = B.length word
       targetWordLength = length targetWord
 
 
 doTargetSearch :: String -> String -> IO ()
 doTargetSearch targetWord (central : []) =
-    readFile wordFile >>= putStrLn . unlines . filter predicate . words
-    where predicate = fitsTarget targetWord central
-
+    B.readFile wordFile >>= B.putStrLn . byteUnlines . filter predicate . byteLines
+    where predicate = fitsTarget (map c2w targetWord) (c2w central)
 
 
 main = do
